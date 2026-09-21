9f6108f54b84aa1dd9f904975e50be17384900e7
tangxingpeng <48871050+edynasty@users.noreply.github.com>
2026-09-21 21:29:01 +0800

    ui: load shared desktop primitive styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./components/primitives/primitives.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
